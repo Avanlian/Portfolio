d01e6ff256a05d895823b721d8ae9edb926f8825
Anthony Vanlian <145938306+Avanlian@users.noreply.github.com>
2025-09-04 15:10:36 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
 plugins: [react()],
-base: '/React-Portfolio/',
+base: '/Portfolio/',
 css: { modules: { localConvention: "camelcase" } },
 });
